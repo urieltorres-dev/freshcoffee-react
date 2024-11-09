088a932966c121df70c80962f6fe0274676bdd9a
Uriel Torres <urieltorres.dev@gmail.com>
2024-11-08 19:05:08 -0600

    Se conectó con la api para consultar las categorías

--- src/context/CafeteriaProvider.jsx
+++ src/context/CafeteriaProvider.jsx
@@ -1,13 +1,13 @@
 import { createContext, useState, useEffect } from 'react'
 import { toast } from 'react-toastify'
-import { categorias as categoriasDB } from '../data/categorias'
+import clienteAxios from '../config/axios'
 
 const CafeteriaContext = createContext()
 
 const CafeteriaProvider = ({children}) => {
 
-    const [categorias, setCategorias] = useState(categoriasDB)
-    const [categoriaActual, setCategoriaActual] = useState(categorias[0])
+    const [categorias, setCategorias] = useState([])
+    const [categoriaActual, setCategoriaActual] = useState({})
     const [modal, setModal] = useState(false)
     const [producto, setProducto] = useState({})
     const [pedido, setPedido] = useState([])
@@ -17,6 +17,20 @@ const CafeteriaProvider = ({children}) => {
         const nuevoTotal = pedido.reduce((total, producto) => (producto.precio * producto.cantidad) + total, 0)
         setTotal(nuevoTotal)
     }, [pedido])
+
+    const obtenerCategorias = async () => {
+        try {
+            const { data } = await clienteAxios('/api/categorias')
+            setCategorias(data.data)
+            setCategoriaActual(data.data[0])
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() =>{
+        obtenerCategorias()
+    }, [])
 
     const handleClickCategoria = id => {
         const categoria = categorias.filter(categoria => categoria.id === id)[0]
