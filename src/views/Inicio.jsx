39c0d22572f3ed2cd42186bbcbbf9f55a0346e19
Uriel Torres <urieltorres.dev@gmail.com>
2024-11-16 19:59:06 -0600

    Se conectó con la api para consultar los productos

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,19 +1,30 @@
+import useSWR from 'swr'
 import Producto from '../components/Producto'
-import { productos as data } from '../data/productos'
 import useCafeteria from '../hooks/useCafeteria'
+import clienteAxios from '../config/axios'
 
 export default function Inicio() {
 
     const { categoriaActual } = useCafeteria()
 
-    const productos = data.filter(producto => producto.categoria_id === categoriaActual.id)
+    const fetcher = () => clienteAxios('/api/productos').then(data => data.data)
+
+    const { data, error, isLoading } = useSWR('/api/productos', fetcher, {
+        refreshInterval: 1000
+    })
+
+    if (isLoading) return <p>Cargando...</p>
+
+    if (error) return <p>Error al cargar los productos.</p>
+
+    const productos = data.data.filter(producto => producto.categoria_id === categoriaActual.id)
 
     return (
         <>
             <h1 className='text-4xl font-black'>{categoriaActual.nombre}</h1>
             <p className='text-2xl my-10'>
                 Personaliza tu pedido.
-            </p>
+            </p>    
 
             <div className='grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
                 {productos.map(producto => (
